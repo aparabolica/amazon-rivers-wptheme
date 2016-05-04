2a4a2b78074f197fb18caf390ec0429cebed174b
Miguel Peixe <miguel@cardume.art.br>
2016-05-04 11:42:25 -0300

    javascript carousel

--- js/site.js
+++ js/site.js
@@ -17,7 +17,37 @@
     }
   });
 
-  // Basin
+  // Carousel
+  $(document).ready(function() {
+    var carousels = $('#carousel');
+    if(carousels.length) {
+      carousels.each(function() {
+        var carousel = $(this);
+        carousel.find('.carousel-item').hide();
+        carousel.find('nav a').on('click', function(e) {
+          e.preventDefault();
+          clearInterval(rotateInterval);
+          var postID = $(this).data('postid');
+          carousel.find('nav a').removeClass('active');
+          carousel.find('.carousel-item').hide();
+          $(this).addClass('active');
+          carousel.find('.carousel-item[data-postid="' + postID + '"]').show();
+          rotateInterval = setInterval(rotate, 8000);
+        });
+        var rotate = function() {
+          if(carousel.find('nav a.active').next('a').length)
+            carousel.find('nav a.active').next('a').click();
+          else
+            carousel.find('nav a:first').click();
+        };
+        var rotateInterval = setInterval(rotate, 8000);
+        carousel.find('nav a:first').click();
+      });
+    }
+  })
+
+
+  // Basin selector
   $(document).ready(function() {
     var selector = $('#basins-selector');
     if(selector.length) {
