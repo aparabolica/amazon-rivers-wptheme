ec0318ddec55e6a4f1fbc5d61f5ead12c808448d
Miguel Peixe <miguel@cardume.art.br>
2016-05-06 12:16:34 -0300

    add missing templates (start archive page)

---
+++ archive.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+
+<section id="archive">
+  <header class="archive-header">
+    <div class="container">
+      <div class="nine columns">
+        <h1><?php
+					if( is_tag() || is_category() || is_tax() ) :
+						printf( __( '%s', 'arp' ), single_term_title() );
+          elseif( is_search() ) :
+            printf( __( 'Search results for: %s', 'arp' ), $_GET['s'] );
+					elseif ( is_day() ) :
+						printf( __( 'Daily Archives: %s', 'arp' ), get_the_date() );
+					elseif ( is_month() ) :
+						printf( __( 'Monthly Archives: %s', 'arp' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'arp' ) ) );
+					elseif ( is_year() ) :
+						printf( __( 'Yearly Archives: %s', 'arp' ), get_the_date( _x( 'Y', 'yearly archives date format', 'arp' ) ) );
+					else :
+						_e( 'Archives', 'arp' );
+					endif;
+				?></h1>
+      </div>
+    </div>
+  </header>
+  <section class="archive-content">
+    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+      <article id="post-<?php the_ID(); ?>">
+        <div class="container">
+          <div class="eight columns">
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <?php the_excerpt(); ?>
+          </div>
+        </div>
+      </article>
+    <?php endwhile; endif; ?>
+  </section>
+</section>
+
+<?php get_footer(); ?>
