0efcb5bcec40b59c93f6b3fd8ef4afc9f6de064f
miguelpeixe <miguel@cardume.art.br>
2016-11-22 11:19:08 -0200

    add dynamic socialinks to library page

--- library.php
+++ library.php
@@ -94,8 +94,20 @@
       <section id="share" class="page-section">
         <h2 class="section-title"><?php _e('Stay tuned', 'arp'); ?></h2>
         <div class="section-box">
-          <a class="fa fa-facebook" href="https://www.facebook.com/WWFLivingAmazonInitiative/" target="_blank" rel="extenal" title="Facebook"></a>
-          <a class="fa fa-youtube" href="https://www.youtube.com/user/LivingAmazon" target="_blank" rel="external" title="YouTube"></a>
+          <?php
+          $fb = get_field('site_options_facebook', 'option');
+          $tw = get_field('site_options_twitter', 'option');
+          $yt = get_field('site_options_youtube', 'option');
+          ?>
+          <?php if($fb) : ?>
+            <a class="fa fa-facebook" href="<?php echo $fb; ?>" target="_blank" rel="extenal" title="Facebook"></a>
+          <?php endif; ?>
+          <?php if($yt) : ?>
+            <a class="fa fa-youtube" href="<?php echo $yt; ?>" target="_blank" rel="external" title="YouTube"></a>
+          <?php endif; ?>
+          <?php if($tw) : ?>
+            <a class="fa fa-twitter" href="<?php echo $tw; ?>" target="_blank" rel="external" title="Twitter"></a>
+          <?php endif; ?>
         </div>
       </section>
       <section id="join" class="page-section">
