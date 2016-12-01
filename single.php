d1429df8d989f80934ab5710acfa853e162f3432
miguelpeixe <miguel@cardume.art.br>
2016-12-01 17:26:27 -0200

    add social links to single page and footer

--- single.php
+++ single.php
@@ -26,6 +26,25 @@
                 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
               </li>
           </aside>
+          <aside class="social">
+            <div class="social-content">
+              <h3><?php _e('Connect with us', 'arp'); ?></h3>
+              <?php
+              $fb = get_field('site_options_facebook', 'option');
+              $tw = get_field('site_options_twitter', 'option');
+              $yt = get_field('site_options_youtube', 'option');
+              ?>
+              <?php if($fb) : ?>
+                <a class="fa fa-facebook" href="<?php echo $fb; ?>" target="_blank" rel="extenal" title="Facebook"></a>
+              <?php endif; ?>
+              <?php if($yt) : ?>
+                <a class="fa fa-youtube" href="<?php echo $yt; ?>" target="_blank" rel="external" title="YouTube"></a>
+              <?php endif; ?>
+              <?php if($tw) : ?>
+                <a class="fa fa-twitter" href="<?php echo $tw; ?>" target="_blank" rel="external" title="Twitter"></a>
+              <?php endif; ?>
+            </div>
+          </aside>
         </div>
       </div>
     </header>
